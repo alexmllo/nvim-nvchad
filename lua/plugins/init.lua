563df72649e30cbfedcac35e1da6d02a1aecaa91
alexmllo <amitjansllorach1@gmail.com>
2024-08-15 20:19:33 +0200

    feat: change structure

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,74 +1,11 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            require("configs.treesitter")
-        end,
-    },
-    {
-        "stevearc/conform.nvim",
-        event = "BufWritePre", -- uncomment for format on save
-        opts = require("configs.conform"),
-    },
-    {
-        "zapling/mason-conform.nvim",
-        event = "VeryLazy",
-        dependencies = { "conform.nvim" },
-        config = function()
-            require("configs.mason-conform")
-        end,
-    },
-    {
-        "mfussenegger/nvim-lint",
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            require("configs.lint")
-        end,
-    },
-    {
-        "rshkarin/mason-nvim-lint",
-        event = "VeryLazy",
-        dependencies = { "nvim-lint" },
-        config = function()
-            require("configs.mason-lint")
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            require("nvchad.configs.lspconfig").defaults()
-            require("configs.lspconfig")
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        event = "VeryLazy",
-        dependencies = { "nvim-lspconfig" },
-        config = function()
-            require("configs.mason-lspconfig")
-        end,
-    },
-    {
-        "kdheepak/lazygit.nvim",
-        cmd = {
-            "LazyGit",
-            "LazyGitConfig",
-            "LazyGitCurrentFile",
-            "LazyGitFilter",
-            "LazyGitFilterCurrentFile",
-        },
-        -- optional for floating window border decoration
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-    },
-    {
-        "rmagatti/auto-session",
-        lazy = false,
-        config = function()
-            require("configs.auto-session")
-        end,
-    },
+    require("plugins.treesitter"),
+    require("plugins.conform"),
+    require("plugins.mason-conform"),
+    require("plugins.lint"),
+    require("plugins.mason-lint"),
+    require("plugins.lspconfig"),
+    require("plugins.mason-lspconfig"),
+    require("plugins.lazygit"),
+    require("plugins.auto-session"),
 }
